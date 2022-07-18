4ffe808d4130a42304999aceaa18f3f42c041bc3
alliu14 <alan.liu@ingka.com>
2022-07-18 11:43:43 +0800

    add comment for escapeAnalysis

--- src/main/java/self/alan/EscapeAnalysis.java
+++ src/main/java/self/alan/EscapeAnalysis.java
@@ -7,28 +7,32 @@ package self.alan;
  * @Created by liuhao
  */
 public class EscapeAnalysis {
-    //全局成员变量逃逸
-    public static Object object;
 
-    //全局成员变量赋值逃逸
-    public  void globalVariableEscape(){
-        object = new Object();
-    }
+  //全局成员变量逃逸
+  public static Object object;
 
-    //方法返回值逃逸
-    public Object methodEscape(){
-        return new Object();
-    }
+  //全局成员变量赋值逃逸
+  public void globalVariableEscape() {
+      //返回的对象逃出了这个方法
+    object = new Object();
+  }
 
-    //实例引用发生逃逸
-    public  void instancePassEscape(){
-        this.speak(this);
-    }
+  //方法返回值逃逸
+  public Object methodEscape() {
+      //方法的返回值 被返回出去可以在外部使用存在逃逸
+    return new Object();
+  }
+
+  //实例引用发生逃逸
+  public void instancePassEscape() {
+      //this对象可以在另一个方法中使用存在逃逸
+    this.speak(this);
+  }
 
 
-    public void  speak(EscapeAnalysis escapeAnalysis){
-        System.out.println("输出.............");
-    }
+  public void speak(EscapeAnalysis escapeAnalysis) {
+    System.out.println("输出.............");
+  }
 
 
 }
